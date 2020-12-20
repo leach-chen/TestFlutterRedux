ce62901dcdfa1756918c75932ca0801626ca958a
leach-chen <349763031@qq.com>
2020-12-20 11:19:43 +0800

    delete file

--- lib/other/store/Store.dart
+++
@@ -1,8 +0,0 @@
-
-
-import 'package:TestFlutterRedux/states/state1/CountState1Redux.dart';
-import 'package:redux/redux.dart';
-
-createStore(){
-   //final store = Store<CountState1>(reducer, initialState: {});
-}
--- lib/states/state1/CountState1Types.dart
+++
@@ -1,1 +0,0 @@
-enum Action1 { increment }
